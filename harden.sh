5662d4828478c66db9f63dfc8bbcf411304e725c
Sai Putravu <aputravu1234@gmail.com>
2021-04-24 19:58:26 +0100

    Format of code

--- harden.sh
+++ harden.sh
@@ -21,12 +21,15 @@ CLEARSCREEN=$(tput clear)
 # Resets everything
 RESET=$(tput sgr 0)
 
+# Global variables
+USERS=$(grep -E "/bin/.*sh" /etc/passwd | grep -v -e root -e `whoami` -e speech-dispatcher | cut -d":" -f1)
+
+
+# -------------------- User functions -------------------- 
 delete_unauthorised_users () {
     # Files necessary: 
     #   * users.txt
 
-    local USERS=$(grep -E "/bin/.*sh" /etc/passwd | grep -v -e root -e `whoami`| cut -d":" -f1)
-    
     echo -e $USERS | sed "s/ /\\n/g" > accusers.txt
     local INVALID=$(diff -n --suppress-common-lines users.txt accusers.txt | cut -d" " -f5-)
 
@@ -60,10 +63,51 @@ add_new_users () {
     echo -n "${CLEARSCREEN}"
     echo "${RED}${BOLD}Type 'exit' to stop.${RESET}"
     while read -r -p "Username to create: " && [[ $REPLY != exit ]]; do 
+        echo "${GREEN}[+] Added new user '${REPLY}'${RESET}"
         sudo useradd $REPLY
     done
 }
 
+change_users_passwords () {
+    # Changes everyone's password to CyberPatriot1!
+    for user in $USERS
+    do 
+        echo $i:'CyberPatriot1!' | sudo chpasswd 
+    done
+}
+
+users_check_uid_0 () {
+    # Checks for any UID 0 users in passwd 
+
+    local UIDS=$(cut /etc/passwd -d: -f1,3 | grep -v root)
+    for i in $UIDS
+    do 
+        local username=$(echo $i | cut -d: -f1)
+        local user_uid=$(echo $i | cut -d: -f2)
+        if [[ $user_uid -eq "0" ]]
+        then 
+            echo "${RED}${BOLD}Found a root UID user [${username} : uid ${user_uid}] !${RESET}"
+            read -rp $'Press <enter> to continue\n'
+        fi
+    done
+}   
+
+check_shadow_password () { 
+    # Checks for any users with an empty password
+    local SHADOW=$(sudo cat /etc/shadow)
+    for line in $SHADOW
+    do 
+        local password_hash=$(echo $line | cut -d: -f2)
+        local account=$(echo $line | cut -d: -f1)
+        if [[ -z $password_hash  ]]
+        then 
+            echo "${RED}${BOLD}Empty password${RESET}${RED} for account ${RED}${BOLD}${account}${RESET}${RED} found in ${RED}/etc/shadow!${RESET}"
+            read -rp $'Press <enter> to continue\n'
+        fi
+    done
+}
+
+# -------------------- APT functions -------------------- 
 autoupdate () {
     sudo apt install unattended-upgrades
 }
@@ -107,3 +151,74 @@ remove_malware () {
         sudo apt purge -y --force-yes $i
     done
 }
+
+
+# -------------------- Main functions -------------------- 
+main_users () {
+    echo -n ${CLEARSCREEN}
+
+    local answer=""
+    echo -n "${CYAN}Delete unauthorised users [${GREEN}y${CYAN}|${RED}N${CYAN}] : ${RESET}"
+    read -rp "" answer
+    case $answer in 
+        y|Y)
+            echo 
+            echo "${YELLOW}You will need to create a file called users.txt with all authorised users.${RESET}"
+            echo "${CYAN}Press <enter> after adding ${BOLD}./users.txt${RESET}"
+            read -r
+            echo "${GREEN}[*] Deleting unauthorised users...${RESET}"
+            delete_unauthorised_users
+            ;;
+        n|N)
+            ;; # Do nothing
+    esac
+
+    echo -n "${CYAN}Delete unauthorised sudoers [${GREEN}y${CYAN}|${RED}N${CYAN}] : ${RESET}"
+    read -rp "" answer
+    case $answer in 
+        y|Y)
+            echo
+            echo "${YELLOW}You will need to create a file called sudoers.txt with all authorised users.${RESET}"
+            echo "${CYAN}Press <enter> after adding ${BOLD}./sudoers.txt${RESET}"
+            read -r
+            echo "${GREEN}[*] Deleting unauthorised sudoers...${RESET}"
+            delete_unauthorised_sudoers
+            ;;
+        n|N)
+            ;; # Do nothing
+    esac
+
+    echo -n "${CYAN}Add users [${GREEN}y${CYAN}|${RED}N${CYAN}] : ${RESET}"
+    read -rp "" answer
+    case $answer in 
+        y|Y)
+            echo 
+            add_new_users 
+            ;;
+        n|N)
+            ;; # Do nothing
+    esac
+
+    echo "${GREEN}[*] Changing every user's password to" 'CyberPatriot1!'"${RESET}"
+    change_users_passwords
+
+    echo "${GREEN}[*] Checking for any UID 0 users ... ${RESET}"
+    users_check_uid_0
+
+    echo "${GREEN}[*] Checking for any users with empty passwords ... ${RESET}"
+    check_shadow_password
+
+    # Order ran 
+    # delete_unauthorised_users
+    # delete_unauthorised_sudoers
+    # add_new_users
+    # change_users_passwords
+    # users_check_uid_0
+    # check_shadow_password
+}
+
+# Function to run everything
+main () {
+    main_users
+}
+main

f32eb40c0d52bd01773ff5d038daec18691a2a22
Sai Putravu <32431279+saiputravu@users.noreply.github.com>
2021-04-24 14:03:21 +0100

    Create harden.sh

---
+++ harden.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+autoupdate () {
+    sudo apt install unattended-upgrades
+}
+
+update () {
+    sudo apt update && sudo apt upgrade
+}
+
+enumerate_packages () {
+    apt list --installed
+}
+
+remove_malware () {
+    declare -a arr=(john, abc, sqlmap, aria2
+                    aquisition, bitcomet, bitlet, bitspirit
+                    endless-sky, zenmap, minetest, minetest-server
+                    armitage, crack, apt pureg knocker, aircrack-ng
+                    airbase-ng, hydra, freeciv
+                    wireshark, tshark
+                    hydra-gtk, netcat, netcat-traditional, netcat-openbsd
+                    netcat-ubuntu, netcat-minimal, qbittorrent, ctorrent
+                    ktorrent, rtorrent, deluge, transmission-common
+                    transmission-bittorrent-client, tixati, frostwise, vuse
+                    irssi, transmission-gtk, utorrent, kismet
+                    medusa, telnet, exim4, telnetd
+                    bind9, crunch, tcpdump, tomcat
+                    tomcat6, vncserver, tightvnc, tightvnc-common
+                    tightvncserver, vnc4server, nmdb, dhclient
+                    telnet-server, ophcrack, cryptcat, cups
+                    cupsd, tcpspray, ettercap
+                    wesnoth, snort, pryit
+                    weplab, wireshark, nikto, lcrack
+                    postfix, snmp, icmp, dovecot
+                    pop3, p0f, dsniff, hunt
+                    ember, nbtscan, rsync, freeciv-client-extras
+                    freeciv-data, freeciv-server, freeciv-client-gtk
+                    )
+
+    for i in "${arr[@]}"
+    do
+        sudo apt purge -y --force-yes $i
+    done
+}
